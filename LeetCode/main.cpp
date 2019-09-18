025866a7425c421ce18b2e22bb674c15891987dc
Nguyen Nghia <nghianv@vng.com.vn>
2019-09-18 07:54:02 +0700

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by Nguyen Van Nghia on 9/18/19.
+//  Copyright © 2019 Nguyen Van Nghia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
